c309134139bb12aceb8230eab5ab049e869a5c31
blackhat7911 <mbishal605@gmail.com>
2021-02-20 09:05:48 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,38 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import Card from './Card';
-import NavBar from './NavBar';
-import Bg from './assets/bg.jpg';
-import Cycle from './assets/cycling.jpeg';
-import TeamZenith from './assets/team_zenith.png';
-import LandingPage from './assets/landing page.png';
+
 
 function App() {
 
-  const [items, setItems] = useState([
-    { title: "Background", desc: "Hulululululu", image: Bg },
-    { title: "Cycling", desc: "Ulululululul", image: Cycle },
-    { title: "Team Zenith", desc: "Kolalalalala", image: TeamZenith },
-    { title: "Landing Page", desc: "Kolakolakola", image: LandingPage },
-  ]);
-
   return (
     <div className="app">
-      <div className="container">
-        <NavBar />
-        <div className="row mt-3">
-          {       
-            items.map((item, index) => (
-              <Card
-                index={index}
-                title={item.title}
-                desc={item.desc}
-                image={item.image}
-              />
-            ))
-          }
-      </div>
-      </div>
+      
     </div>
   );
 }
